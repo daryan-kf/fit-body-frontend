eafa47f8543b4513db56264fe1bc2844c2fde9a1
daryan-kf <daryankamalifar@gmail.com>
2025-03-22 22:43:28 +0100

    Enhance Home component with user session management and background image; add AVIF type declaration for image imports

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,48 @@
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect } from 'react';
 import { Link } from 'expo-router';
 import { View, Text, YStack } from 'tamagui';
+import { supabase } from '@/lib/supabase';
+import { Session } from '@supabase/supabase-js';
+
+import backgroundImage from '@/assets/background-images/home-background.avif';
+import { ImageBackground } from 'react-native';
 
 const Home = () => {
+  const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  console.log({ LoggedInUserEmail: session?.user.email });
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: '#FFF'
-      }}
+    <ImageBackground
+      source={backgroundImage}
+      style={{ flex: 1, width: '100%', height: '100%' }}
     >
-      <YStack justifyContent="center" alignItems="center" padding="40px">
-        <Text>Home Screen</Text>
-        <Link href="/login">
-          <Text>Login</Text>
-        </Link>
-        <Link href="/signup">
-          <Text>Signup</Text>
-        </Link>
-      </YStack>
-    </View>
+      <View
+        flex={1}
+        backgroundColor="rgba(0, 0, 0, 0.5)" // 50% opacity black overlay
+        justifyContent="center"
+        alignItems="center"
+      >
+        <YStack justifyContent="center" alignItems="center" padding="40px">
+          <Text>Home Screen</Text>
+          <Link href="/login">
+            <Text>Login</Text>
+          </Link>
+          <Link href="/signup">
+            <Text>Signup</Text>
+          </Link>
+        </YStack>
+      </View>
+    </ImageBackground>
   );
 };
 
---
+++ types/images.d.ts
@@ -0,0 +1,4 @@
+declare module '*.avif' {
+  const content: number;
+  export default content;
+}
