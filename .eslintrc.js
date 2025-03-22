60ab0db754380e442dc96b11df2ed0c201d11a73
daryan-kf <daryankamalifar@gmail.com>
2025-03-22 11:37:29 +0100

    Refactor ESLint and Prettier configurations for improved code quality and consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
   root: true,
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-native/all",
-    "prettier",
+    'expo',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react", "react-native", "prettier"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": ["error"],
-    "react/react-in-jsx-scope": "off", // Not needed for React Native
-    "react-native/no-inline-styles": "off", // Allow inline styles if necessary
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'warn',
+    semi: ['error', 'always'],
+    'prefer-const': 'error'
+  }
 };
