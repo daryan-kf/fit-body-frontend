29977556ba71f493616696d2f09eadcfd13d67c7
daryan-kf <daryankamalifar@gmail.com>
2025-03-22 22:45:02 +0100

    use supabase and create fully functioning login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import { Link } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
 import { View, Text } from 'tamagui';
+
+import { supabase } from '@/lib/supabase';
 
 import { TextField, Button } from '@/components';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password
+    });
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
+
   return (
     <View flex={1} paddingHorizontal="$6" paddingVertical="$8">
       <Text color="#DAF029" marginBottom="$8">
@@ -19,22 +37,41 @@ const Login = () => {
         tempor incididunt ut labore et dolore magna aliqua.
       </Text>
 
-      <TextField placeholder="example@example.com" marginBottom="$4" />
+      <TextField
+        placeholder="example@example.com"
+        marginBottom="$4"
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
 
-      <TextField placeholder="enter password" marginBottom="$4" />
+      <TextField
+        placeholder="enter password"
+        marginBottom="$4"
+        onChangeText={text => setPassword(text)}
+        value={password}
+      />
 
       <Text color="#FAF923" fontSize={14} textAlign="right" marginBottom="$4">
         Forgot Password?
       </Text>
 
-      <Button marginBottom="$2">SIGN IN</Button>
+      <Button
+        marginBottom="$2"
+        disabled={loading}
+        onPress={() => signInWithEmail()}
+      >
+        SIGN IN
+      </Button>
 
       <Text color="#999999" fontSize={14} textAlign="center" marginBottom="$2">
         or sign in with
       </Text>
 
       <Text color="#999999" fontSize={14} textAlign="center">
-        Don&apos;t have an account? <Text color="#DAF029">Sign Up</Text>
+        Don&apos;t have an account?{' '}
+        <Link href="/signup">
+          <Text color="#DAF029">Sign Up</Text>
+        </Link>
       </Text>
     </View>
   );
