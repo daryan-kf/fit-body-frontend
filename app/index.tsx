d8df66447742d8100310f58fd40473f386a14e07
daryan-kf <daryankamalifar@gmail.com>
2025-03-24 21:46:04 +0100

    Update Home component to enhance welcome text and improve background overlay opacity; adjust link text styling for better user experience

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
-import 'react-native-url-polyfill/auto';
 import { useState, useEffect } from 'react';
+import { ImageBackground } from 'react-native';
 import { Link } from 'expo-router';
-import { View, Text, YStack } from 'tamagui';
-import { supabase } from '@/lib/supabase';
+import { View, YStack } from 'tamagui';
+import 'react-native-url-polyfill/auto';
+
 import { Session } from '@supabase/supabase-js';
+import { supabase } from '@/lib/supabase';
+
+import { Text } from '@/components';
 
 import backgroundImage from '@/assets/background-images/home-background.avif';
-import { ImageBackground } from 'react-native';
 
 const Home = () => {
   const [session, setSession] = useState<Session | null>(null);
@@ -28,17 +31,20 @@ const Home = () => {
     >
       <View
         flex={1}
-        backgroundColor="rgba(0, 0, 0, 0.5)" // 50% opacity black overlay
+        backgroundColor="rgba(0, 0, 0, 0.8)"
         justifyContent="center"
         alignItems="center"
       >
         <YStack justifyContent="center" alignItems="center" padding="40px">
-          <Text>Home Screen</Text>
+          <Text>Welcome to</Text>
+          <Text variant="title">FitBody</Text>
+
           <Link href="/login">
-            <Text>Login</Text>
+            <Text variant="link">Login</Text>
           </Link>
+          <Text>or</Text>
           <Link href="/signup">
-            <Text>Signup</Text>
+            <Text variant="link">Signup</Text>
           </Link>
         </YStack>
       </View>
