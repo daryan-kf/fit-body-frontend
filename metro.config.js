9a58e157385d9b05e8e71f286ecd35471b720c4b
daryan-kf <daryankamalifar@gmail.com>
2025-03-25 20:19:30 +0100

    Add metro.config.js to configure SVG handling with react-native-svg-transformer

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer')
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg']
+};
+
+module.exports = config;
